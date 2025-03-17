f1fb8d1ad62108f87d0967d685b775f500648d34
haibui72 <haibuai3@gmail.com>
2025-03-18 01:18:40 +0700

    update

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-const words = ["Hai", "an AI Engineer", "a Tech Enthusiast", "a Developer"];
-let i = 0;
-let j = 0;
-let currentWord = "";
-let isDeleting = false;
-const typingElement = document.querySelector(".typing");
-
-function typeEffect() {
-    currentWord = words[i];
-
-    if (isDeleting) {
-        typingElement.textContent = currentWord.substring(0, j--);
-    } else {
-        typingElement.textContent = currentWord.substring(0, j++);
-    }
-
-    if (!isDeleting && j === currentWord.length + 1) {
-        isDeleting = true;
-        setTimeout(typeEffect, 1000);
-    } else if (isDeleting && j === 0) {
-        isDeleting = false;
-        i = (i + 1) % words.length;
-        setTimeout(typeEffect, 500);
-    } else {
-        setTimeout(typeEffect, 100);
-    }
-}
-
-document.addEventListener("DOMContentLoaded", typeEffect);
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1000, // Animation duration
+        easing: "ease-in-out",
+    });
+});
